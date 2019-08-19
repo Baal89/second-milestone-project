be7e7302a54dc6054d1c1113e1edcf9f0088b870
Ubuntu <ubuntu@ip-172-31-28-76.eu-west-1.compute.internal>
2019-08-19 15:52:19 +0000

    mother education chart plotted

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,6 +8,7 @@ function makeGraphs(error, performanceData) {
     show_gender_balance(ndx);
     show_age_balance(ndx);
     show_family_size(ndx);
+    show_mother_education(ndx);
 
     dc.renderAll();
 }
@@ -65,3 +66,58 @@ function show_family_size(ndx) {
         .xAxisLabel("Students family size")
         .yAxis().ticks(20);
 }
+
+function show_mother_education(ndx) {
+
+    function graduationBYMotherEducation(dimension, Medu) {
+        return dimension.group().reduce(
+            function(p, v) {
+                p.total++;
+                if (v.rank == Medu) {
+                    p.match++;
+                }
+                return p;
+            },
+            function(p, v) {
+                p.total--;
+                if (v.rank == Medu) {
+                    p.match--;
+                }
+                return p;
+            },
+            function() {
+                return { total: 0, match: 0 };
+            }
+        );
+    }
+
+    var dim = ndx.dimension(dc.pluck("G3"));
+    var noneMotherEducation = graduationBYMotherEducation(dim, "0");
+    var PrimaryMotherEducation = graduationBYMotherEducation(dim, "1");
+    var secondaryMotherEducation = graduationBYMotherEducation(dim, "2");
+    var secondaryGradeMotherEducation = graduationBYMotherEducation(dim, "3");
+    var higherMotherEducation = graduationBYMotherEducation(dim, "4");
+
+    dc.barChart("#mother-education")
+        .width(400)
+        .height(300)
+        .dimension(dim)
+        .group(noneMotherEducation, "0")
+        .stack(PrimaryMotherEducation, "1")
+        .stack(secondaryMotherEducation, "2")
+        .stack(secondaryGradeMotherEducation, "3")
+        .stack(higherMotherEducation, "4")
+        .valueAccessor(function(d) {
+            if (d.value > 0) {
+                return (d.value.match / d.value.total) * 100;
+            }
+            else {
+                return 0;
+            }
+        })
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
+        .margins({ top: 10, right: 100, bottom: 30, left: 30 });
+}
+
